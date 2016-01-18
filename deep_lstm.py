9c20595244f54a73467c359ccd3b2c311816c9f2
Taehoon Kim <carpedm20@gmail.com>
2016-01-19 01:21:52 +0900

    update deep_lstm

--- deep_lstm.py
+++ deep_lstm.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.models.rnn import rnn, rnn_cell
 
+vocab_size = 100000
 size = 256 # 64, 128, 256
 learning_rate = 1e-4 # 1e-3, 5e-4, 1e-4, 5e-5
 batch_size = 32 # 16, 32
@@ -9,9 +10,11 @@ max_time_unit = 100
 
 inputs = []
 for idx in xrange(max_time_unit):
-  inputs.append(tf.placeholder(tf.float32, []))
+  inputs.append(tf.placeholder(tf.float32, [batch_size, vocab_size]))
 
 cell_fw = rnn_cell.BasicLSTMCell(size)
 cell_bw = rnn_cell.BasicLSTMCell(size)
 
-outputs = rnn.bidirectional_rnn(cell_fw, cell_bw, inputs)
+outputs = rnn.bidirectional_rnn(cell_fw, cell_bw, inputs,
+                                dtype=tf.float32,
+                                sequence_length=[5]*batch_size)
