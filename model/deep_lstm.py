951f66be9801ebc8a86c9873f38a45456724fe81
Taehoon Kim <carpedm20@gmail.com>
2016-01-23 18:12:47 +0900

    deep_lstm finished

--- model/deep_lstm.py
+++ model/deep_lstm.py
@@ -7,7 +7,7 @@ class DeepLSTM(Model):
   """Deep LSTM model."""
   def __init__(self, vocab_size, size=256,
                learning_rate=1e-4, batch_size=32,
-               dropout=0.1, max_time_unit=100,
+               dropout=0.1, seq_length=100,
                checkpoint_dir="checkpoint", forward_only=False):
     """Initialize the parameters for an Deep LSTM model.
     
@@ -17,7 +17,7 @@ class DeepLSTM(Model):
       learning_rate: float, [1e-3, 5e-4, 1e-4, 5e-5]
       batch_size: int, The size of a batch [16, 32]
       dropout: unit Tensor or float between 0 and 1 [0.0, 0.1, 0.2]
-      max_time_unit: int, The max time unit [100]
+      seq_length: int, The max time unit [100]
     """
     super(DeepLSTM, self).__init__()
 
@@ -26,27 +26,20 @@ class DeepLSTM(Model):
     self.learning_rate = float(learning_rate)
     self.batch_size = int(batch_size)
     self.dropout = float(dropout)
-    self.max_time_unit = int(max_time_unit)
+    self.seq_length = int(seq_length)
 
-    self.inputs = []
+    self.inputs = tf.placeholder(tf.int32, [self.batch_size, self.seq_length])
+    self.input_lengths = tf.placeholder(tf.int64, [self.batch_size])
 
     self.emb = tf.Variable(tf.truncated_normal([self.vocab_size, self.size], -0.1, 0.1), name='emb')
+    self.embed_inputs = tf.nn.embedding_lookup(self.emb, tf.transpose(self.inputs))
 
-    for idx in xrange(max_time_unit):
-      #self.inputs.append(tf.placeholder(tf.float32, [self.batch_size, vocab_size]))
-      self.inputs.append(tf.nn.embedding_lookup(self.emb,
-                                                tf.placeholder(tf.int32, 1)))
-
-    self.cell_fw = rnn_cell.BasicLSTMCell([1, size])
-    self.cell_bw = rnn_cell.BasicLSTMCell([1, size])
-    self.state_fw = tf.zeros([1, size])
-    self.state_bw = tf.zeros([1, size])
+    self.cell_fw = rnn_cell.BasicLSTMCell(size)
+    self.cell_bw = rnn_cell.BasicLSTMCell(size)
 
   def build_model(self, sequence_length):
     return rnn.bidirectional_rnn(self.cell_fw,
                                  self.cell_bw,
-                                 self.inputs,
-                                 #dtype=tf.float32,
-                                 sequence_length=sequence_length,
-                                 initial_state_fw=self.state_fw,
-                                 initial_state_bw=self.state_bw)
+                                 tf.unpack(self.embed_inputs),
+                                 dtype=tf.float32,
+                                 sequence_length=self.input_lengths)
